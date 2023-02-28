ba7735c0f176a09124eb03b66c4698adc5430059
Rachna Sinha <rachnasinha955@gmail.com>
2023-03-01 00:44:09 +0530

    sub files

---
+++ multiply_it.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 28 22:49:46 2023
+
+@author: HP
+"""
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
---
+++ subtract_it.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 28 22:48:03 2023
+
+@author: HP
+"""
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
